7b400d900ea030b5c62c50a4dbf8fa9ab6c2fa9d
Clevis Willrich <clevy.clev@gmail.com>
2017-03-22 21:51:19 -1000

    Initial Commit

---
+++ Hero Finale/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hero Finale
+//
+//  Created by Clevis Necol Valentin Willrich on 3/22/17.
+//  Copyright © 2017 Clevis Necol Valentin Willrich. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
